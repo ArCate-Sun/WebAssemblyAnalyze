762e9a31f0261e2a8a6b12ccf5b3f87884915a8e
acat <sun.arcate@icloud.com>
2019-05-08 17:11:39 +0800

    commit

--- AutoGrabOutput/js/content.js
+++ AutoGrabOutput/js/content.js
@@ -12,14 +12,8 @@ function injectCustomJs(jsPath)
     // };
     document.head.appendChild(temp);
 }
-(function () {
-    var ie =!!(window.attachEvent&&!window.opera),wk=/webkit\/(\d+)/i.test(navigator.userAgent)&&(RegExp.$1<525);
-    var fn =[],run=function(){for(var i=0;i<fn.length;i++)fn[i]();},d=document;d.ready=function(f){
-    if(!ie&&!wk&&d.addEventListener){returnd.addEventListener('DOMContentLoaded',f,false);}if(fn.push(f)>1)return;
-    if(ie)(function(){try{d.documentElement.doScroll('left');run();}catch(err){setTimeout(arguments.callee,0);}})();
-    else if(wk)var t=setInterval(function(){if(/^(loaded|complete)$/.test(d.readyState))clearInterval(t),run();},0);};
- })();
+
 
 window.onload = function() {
-    
+    injectCustomJs()
 }
